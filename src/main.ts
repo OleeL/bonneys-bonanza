4711e1a35d13e23a6e73b411a2b274183e8d846f
Oli <oliglegg@hotmail.co.uk>
2022-04-10 11:24:16 +0100

    upgrade versions + implement tabs 😎

--- src/main.ts
+++ src/main.ts
@@ -1,42 +1,45 @@
 import { app, BrowserWindow } from 'electron';
 import { join } from 'path';
 
+
+console.log(process ?? "process was null")
+
 // Let electron reloads by itself
 if (process.env.ELECTRON_DEBUG === 'true' || process.env.ELECTRON_DEBUG === 'vscode') {
-  // tslint:disable-next-line:no-var-requires
-  require('electron-reload')(__dirname);
+    // tslint:disable-next-line:no-var-requires
+    require('electron-reload')(__dirname);
 }
 
 let mainWindow: Electron.BrowserWindow | null;
 
 function createWindow() {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
-    width: 1400,
-    height: 700,
-    webPreferences: {
-      nodeIntegration: true,
-    },
-  });
+    // Create the browser window.
+    mainWindow = new BrowserWindow({
+        width: 1400,
+        height: 700,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        },
+    });
+    // and load the index.html of the app.
+    mainWindow.loadFile(join(__dirname, 'index.html'));
 
-  // and load the index.html of the app.
-  mainWindow.loadFile(join(__dirname, 'index.html'));
+    if (process.env.ELECTRON_DEBUG === 'true') {
+        // Open the DevTools.
+        mainWindow.webContents.openDevTools();
+    } else if (!process.env.ELECTRON_DEBUG || process.env.ELECTRON_DEBUG === 'false') {
+        // Open window in fullscreen
+        mainWindow.setFullScreen(true);
+    }
 
-  if (process.env.ELECTRON_DEBUG === 'true') {
-    // Open the DevTools.
-    mainWindow.webContents.openDevTools();
-  } else if (!process.env.ELECTRON_DEBUG || process.env.ELECTRON_DEBUG === 'false') {
-    // Open window in fullscreen
-    mainWindow.setFullScreen(true);
-  }
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-  });
+    // Emitted when the window is closed.
+    mainWindow.on('closed', () => {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null;
+    });
 }
 
 // This method will be called when Electron has finished
@@ -46,19 +49,19 @@ app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
 
 app.on('activate', () => {
-  // On OS X it"s common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow();
-  }
+    // On OS X it"s common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+        createWindow();
+    }
 });
 
 // In this file you can include the rest of your app"s specific main process
